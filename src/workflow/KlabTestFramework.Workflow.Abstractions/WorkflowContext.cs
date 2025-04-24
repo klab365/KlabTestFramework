d06266fe63826781f14bb9fade44a4022663c138
BuriKizilkaya <burak.kizilkaya@outlook.com>
2025-04-24 19:53:48 +0200

    Update

--- src/workflow/KlabTestFramework.Workflow.Abstractions/WorkflowContext.cs
+++ src/workflow/KlabTestFramework.Workflow.Abstractions/WorkflowContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using KlabTestFramework.Shared.Parameters;
 
 namespace KlabTestFramework.Workflow.Abstractions.Specifications;
 
@@ -7,6 +9,26 @@ namespace KlabTestFramework.Workflow.Abstractions.Specifications;
 /// </summary>
 public class WorkflowContext
 {
-    private readonly List<IVariable> _runTimeVariables = new();
-    public IEnumerable<IVariable> RunTimeVariables => _runTimeVariables;
+    private readonly Dictionary<string, IParameterType> _runTimeVariables = [];
+    public IReadOnlyDictionary<string, IParameterType> RunTimeVariables => _runTimeVariables;
+
+    /// <summary>
+    /// Adds a runtime variable to the workflow context.
+    /// This is useful for storing temporary data that is needed during the workflow execution.
+    /// The key must be unique within the context, and if a variable with the same key already exists,
+    /// </summary>
+    public void AddRunTimeVariable<TParameter>(string key, TParameter value) where TParameter : IParameterType
+    {
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            throw new ArgumentException("Key cannot be null or whitespace.", nameof(key));
+        }
+
+        if (_runTimeVariables.ContainsKey(key))
+        {
+            throw new ArgumentException($"Key '{key}' already exists in the runtime variables.", nameof(key));
+        }
+
+        _runTimeVariables.Add(key, value);
+    }
 }
